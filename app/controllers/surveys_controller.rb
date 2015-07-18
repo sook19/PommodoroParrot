8dc1c47a0a8f1f2a07565a179e9177133e5b1f00
sook19 <sookdejonge@gmail.com>
2015-07-18 10:59:24 +0000

    Survey form

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -10,6 +10,7 @@ class SurveysController < ApplicationController
   # GET /surveys/1
   # GET /surveys/1.json
   def show
+    @timer = @survey.timer
   end
 
   # GET /surveys/new
--- app/models/survey.rb
+++ app/models/survey.rb
@@ -1,3 +1,9 @@
 class Survey < ActiveRecord::Base
-  has_one :timer 
+  has_one :timer
+
+  after_create :create_timer
+
+  def create_timer
+    Timer.create(survey: self)
+  end
 end
